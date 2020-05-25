c1bde17c6cef96b39c4037550cc073014c0d6076
majorlin <mingjie.lin@nxp.com>
2020-05-25 11:20:19 +0800

    remove flash program code

--- Modules/platinum/main.c
+++ Modules/platinum/main.c
@@ -5,9 +5,6 @@ extern void env_sysinit(void);
 int main(){
     env_sysinit();
     printf("Hello, world!\r\n");
-    GPIOB->PDDR |= 0x3;
-    flash_erase();
-    flash_program();
     flexcan_print_regs(CAN0);
     flexcan_print_regs(CAN1);
     while(1){
